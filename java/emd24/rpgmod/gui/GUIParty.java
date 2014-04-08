c51ad9307d241d3fac25bc10beec6cbb14e38d25
IunRhys <eaharvey@wisc.edu>
2014-04-07 22:51:25 -0500

    Major modifications made to the GUIParty class.
    Recrafted the GUIParty to use a list for better access and improved the
    spawning of buttons. There are still issues to fix, though.

--- java/emd24/rpgmod/gui/GUIParty.java
+++ java/emd24/rpgmod/gui/GUIParty.java
@@ -1,10 +1,13 @@
 package emd24.rpgmod.gui;
 
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.Iterator;
+import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
 import java.util.Set;
+import java.util.ArrayList;
 
 import org.lwjgl.opengl.GL11;
 
@@ -26,6 +29,7 @@ import net.minecraft.client.gui.GuiScreen;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.entity.player.EntityPlayerMP;
+import emd24.rpgmod.party.PartyPlayerNode;
 
 /**
  * This class is the GUI that shows the party interface. Based on code by Evan Dyke.
@@ -41,11 +45,26 @@ public class GUIParty extends GuiScreen {
 	
 	public static final ResourceLocation resource = new ResourceLocation("RPGMod", "textures/gui/");
 	private EntityPlayer player;
+	private int playerPartyID;
 	
-	//private GuiControlsScrollPanel scrollPane;
+	List<PartyPlayerNode> list;
+	private final int pageSize = 7;
+	private int currentPage;
 	
 	HashMap<String, Integer> pAP = PartyManagerClient.playerParty;
-	Set<Map.Entry<String, Integer>> playersAndParties;
+	
+	GuiButton leaveBtn;
+	GuiButton pBtn1, pBtn2, pBtn3, pBtn4, pBtn5, pBtn6, pBtn7;
+	GuiButton kBtn1, kBtn2, kBtn3, kBtn4, kBtn5, kBtn6, kBtn7;
+	GuiButton iBtn1, iBtn2, iBtn3, iBtn4, iBtn5, iBtn6, iBtn7;
+	String kiString1, kiString2, kiString3, kiString4, kiString5, 
+	kiString6, kiString7;
+	GuiButton[] promoteBtns = {pBtn1, pBtn2, pBtn3, pBtn4, pBtn5, pBtn6, pBtn7};
+	GuiButton[] kickBtns = {kBtn1, kBtn2, kBtn3, kBtn4, kBtn5, kBtn6, kBtn7};
+	GuiButton[] inviteBtns = {iBtn1, iBtn2, iBtn3, iBtn4, iBtn5, iBtn6, iBtn7};
+	String promote = "Promote";
+	String kick = "Kick";
+	String invite = "Invite";
 	
 	public GUIParty(EntityPlayer player){
 		this.player = player;
@@ -53,28 +72,116 @@ public class GUIParty extends GuiScreen {
 	
 	public void initGui()
 	{
-		//scrollPane = new GuiControlsScrollPanel(this, options, mc);
-
+		currentPage = 0;
+		
 		buttonList.clear();
-		playersAndParties = pAP.entrySet();
-		Iterator itPaP = playersAndParties.iterator();
-		int h = 1;
-		// This is a janky way of doing this. Look at MC's Control's page to
-		// understand how to actually set this up correctly with a scroll bar.
-		while (itPaP.hasNext()){
-			itPaP.next();
-			buttonList.add(new GuiButton(h, 250, h * 20 + 45, 50, 20, "Invite"));
-			buttonList.add(new GuiButton(h + pAP.size(), 310, h * 20 + 45, 50, 20, "Kick"));
-			h++;
+		
+		list = new ArrayList<PartyPlayerNode>();
+		PartyPlayerNode currNode;
+		Set<Map.Entry<String, Integer>> papEntrySet = pAP.entrySet();
+		Iterator itr = papEntrySet.iterator();
+		while(itr.hasNext()){
+			Map.Entry<String, Integer> entry = (Entry<String, Integer>) itr.next();
+			if (entry.getKey().compareTo(player.getCommandSenderName()) == 0){
+				this.playerPartyID = entry.getValue();
+			}
+			list.add(new PartyPlayerNode(entry.getKey(), entry.getValue()));
 		}
 		
+		Collections.sort(list);
+		// Set up promote and kick/invite buttons
+		int i = 1;
+		for(GuiButton e: promoteBtns){
+			promoteBtns[i - 1] = new GuiButton(i, 250, 20 * i + 30, 50, 20, promote);
+			promoteBtns[i - 1].enabled = false;
+			promoteBtns[i - 1].visible = false;
+			buttonList.add(promoteBtns[i - 1]);
+			i++;
+		}
+		i = 1;
+		for(GuiButton e: kickBtns){
+			kickBtns[i - 1] = new GuiButton(i + pageSize, 310, 20 * i + 30, 50, 20, kick);
+			kickBtns[i - 1].enabled = false;
+			kickBtns[i - 1].visible = false;
+			buttonList.add(kickBtns[i - 1]);
+			i++;
+		}
+		i = 1;
+		for(GuiButton e: inviteBtns){
+			inviteBtns[i - 1] = new GuiButton(i + 2 * pageSize, 310, 20 * i + 30, 50, 20, invite);
+			inviteBtns[i - 1].enabled = false;
+			inviteBtns[i - 1].visible = false;
+			buttonList.add(inviteBtns[i - 1]);
+			i++;
+		}
+		leaveBtn = new GuiButton(0, 90, 195, 100, 20, "Leave Party");
+		buttonList.add(leaveBtn);
+		buttonList.add(new GuiButton(-1, 210, 195, 100, 20, "Done"));		
 	}
 	
 	protected void actionPerformed(GuiButton guibutton)
 	{
 		//We need to take the button id, figure out which player that entry maps
 		//to and then send the invite to the player.
-		Iterator itPaP = playersAndParties.iterator();
+		//TODO: UPDATE THIS TO HANDLE THE NEW LIST
+		String invitingPlayer = Minecraft.getMinecraft().thePlayer.getCommandSenderName();
+		PartyInvitePacket packet;
+		PartyPlayerNode curr;
+		int type;
+		System.out.println("Party guibutton.id: " + guibutton.id);
+		switch(guibutton.id){
+		// Close the gui
+		case -1:
+			this.mc.displayGuiScreen(null);
+			break;
+		// Leave party
+		case 0:
+			type = 2;
+			packet = new PartyInvitePacket(type, invitingPlayer, invitingPlayer);
+			RPGMod.packetPipeline.sendToServer(packet);
+			break;
+		// Promote buttons
+		case 1:
+		case 2:
+		case 3:
+		case 4:
+		case 5:
+		case 6:
+		case 7:
+			// Promote button time stuff
+			break;
+		// Kick buttons
+		case 8:
+		case 9:
+		case 10:
+		case 11:
+		case 12:
+		case 13:
+		case 14:
+			curr = list.get(guibutton.id - pageSize - 1 + currentPage * pageSize);
+			// They're from the same party, so it's a kick.
+			type = 2;
+			packet = new PartyInvitePacket(type, curr.playerName, invitingPlayer);
+			RPGMod.packetPipeline.sendToServer(packet);
+			break;
+		case 15:
+		case 16:
+		case 17:
+		case 18:
+		case 19:
+		case 20:
+		case 21:
+			curr = list.get(guibutton.id - (pageSize * 2) - 1 + currentPage * pageSize);
+			// It's an invite!
+			type = 1;
+			packet = new PartyInvitePacket(type, curr.playerName, invitingPlayer);
+			RPGMod.packetPipeline.sendToServer(packet);
+			break;
+		}
+		
+		
+		/* LEAVING THIS FOR REFERENCE
+		Iterator itPaP = list.iterator();
 		int h = guibutton.id - 1;
 		int type = 1;
 		if(h >= pAP.size()) {
@@ -88,54 +195,78 @@ public class GUIParty extends GuiScreen {
 		String playerName = entry.getKey();
 		
 		//Send packet
-		String invitingPlayer = Minecraft.getMinecraft().thePlayer.getCommandSenderName();
+		//String invitingPlayer = Minecraft.getMinecraft().thePlayer.getCommandSenderName();
 		PartyInvitePacket packet = new PartyInvitePacket(type, playerName, invitingPlayer);
-		RPGMod.packetPipeline.sendToServer(packet);
+		RPGMod.packetPipeline.sendToServer(packet);*/
 	}
 	
 	@Override
 	public void drawScreen(int i, int j, float f){
+		// Draw background and header
 		drawDefaultBackground();
-		
 		drawRect(20, 20, width - 20, height - 20, 0xffdddddd);
-		
 		drawCenteredString(fontRendererObj, "Party", width / 2, 30, 0xffffffff);
 		
-		//Simplified Algo for Testing:
-		//obtain a list of players in the world
-		//for each player
-		//draw name, draw party ID, draw invite button
-		
-		Iterator itPaP = playersAndParties.iterator();
-		int h = 1;
-		while (itPaP.hasNext()){
-			Map.Entry<String, Integer> entry = (Entry<String, Integer>) itPaP.next();
-			drawString(fontRendererObj, entry.getKey(), 50, h * 20 + 50, 0xffffffff);
-			drawString(fontRendererObj, "" + entry.getValue(), 150, h * 20 + 50, 0xffffffff);
-			// buttonList.add(new GuiButton(h, 250, h * 20 + 50, "Invite"));
-			h++;
+		// Check for Leave Party Button
+		if (playerPartyID == 0){
+			leaveBtn.enabled = false;
 		}
-
+		
+		//TODO: Update this to do scrolling list
+		//TODO: Update this to dynamically update partyID.
+		for(int k = 0; k < 7; k++){
+			// Make sure that we have somebody to put in this slot
+			if(k + currentPage * pageSize < list.size()){
+				PartyPlayerNode curr = list.get(k + currentPage * pageSize);
+				drawString(fontRendererObj, curr.playerName, 50, (k + 1) * 20 + 35, 0xffffffff);
+				drawString(fontRendererObj, "" + curr.partyID, 150, (k + 1) * 20 + 35, 0xffffffff);
+				// We are the leader and this person is in our party
+				if(playerPartyID < 0 && Math.abs(playerPartyID) == curr.partyID){
+					promoteBtns[k].enabled = true;
+					promoteBtns[k].visible = true;
+					kickBtns[k].enabled = true;
+					kickBtns[k].visible = true;
+					inviteBtns[k].enabled = false;
+					inviteBtns[k].visible = false;
+				} 
+				// We aren't the leader, but this person is in our party
+				else if (playerPartyID != 0 && playerPartyID == Math.abs(curr.partyID)){ 
+					promoteBtns[k].enabled = false;
+					promoteBtns[k].visible = true;
+					kickBtns[k].enabled = false;
+					kickBtns[k].visible = true;
+					inviteBtns[k].enabled = false;
+					inviteBtns[k].visible = false;
+				} 
+				// Hey! It's ourself.
+				else if (curr.playerName.compareTo(player.getDisplayName()) == 0){
+					promoteBtns[k].enabled = false;
+					promoteBtns[k].visible = false;
+					kickBtns[k].enabled = false;
+					kickBtns[k].visible = false;
+					inviteBtns[k].enabled = false;
+					inviteBtns[k].visible = false;
+				} 
+				// This person isn't any our party
+				else {
+					promoteBtns[k].enabled = false;
+					promoteBtns[k].visible = false;
+					kickBtns[k].enabled = false;
+					kickBtns[k].visible = false;
+					inviteBtns[k].enabled = true;
+					inviteBtns[k].visible = true;
+				}
+			} 
+			// We don't have a list item to fill this spot
+			else {
+				promoteBtns[k].enabled = false;
+				promoteBtns[k].visible = false;
+				kickBtns[k].enabled = false;
+				kickBtns[k].visible = false;
+				inviteBtns[k].enabled = false;
+				inviteBtns[k].visible = false;
+			}
+		}
 		super.drawScreen(i, j, f);
 	}
-	/*
-	if(inParty){
-	putPartyMembersAtFrontOfList
-	drawLeaveButton
-	}
-	draw an entry for each player
-	if(!inOurParty){
-	drawInviteButton
-	} else if(inOurParty){
-	if(weAreLeader){
-	drawPromoteButton
-	drawKickButton
-	}
-	drawLocation
-	}
-	}*/
-	
-	
-	
-	
 }
