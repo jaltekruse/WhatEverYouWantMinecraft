957a6122eea49da3764c2f76809fc34821395a62
Wesley <reardan@gmail.com>
2014-04-11 12:30:07 -0500

    Added unit test class for DialogueTreeNode class

--- java/emd24/rpgmod/quest/DialogueTreeNode.java
+++ java/emd24/rpgmod/quest/DialogueTreeNode.java
@@ -12,19 +12,33 @@ import net.minecraft.client.gui.GuiScreen;
 
 public class DialogueTreeNode {
 	public List<DialogueTreeNode> children;	//List of children dialogue nodes
+	public DialogueTreeNode parent;	//Parent node
 	public String dialogueText;	//Text display to user, or choices if isReply
 	public String itemNeeded;		//The item ID needed to access this dialogue item
 	public int itemQuantity;		//The quantity of items needed to access this dialogue item
 	public boolean isReply;		//if the node is a reply to a statement made by the character
 	public String action;	//use in a factory to create action stuff
 	
-	public DialogueTreeNode() {
+	protected void Init()
+	{
 		children = new ArrayList<DialogueTreeNode>();
 		dialogueText = "";
 		itemNeeded = "";
 		itemQuantity = 0;
 		isReply = false;
 		action = "";
+		parent = null;
+	}
+	
+	public DialogueTreeNode()
+	{
+		Init();
+	}
+	
+	public DialogueTreeNode(DialogueTreeNode parent)
+	{
+		Init();
+		this.parent = parent;
 	}
 	
 	/*
@@ -76,7 +90,7 @@ public class DialogueTreeNode {
 	/**
 	 * Recursive load method for dialogue tree
 	 * 
-	 * @return resuling line number
+	 * @return resulting line number
 	 */
 	protected int load(String[] lines, int lineNumber, String spaces) {
 		String line = lines[lineNumber].trim();
@@ -122,5 +136,9 @@ public class DialogueTreeNode {
 			//nodes.add(child);
 			child.getList(nodes);
 		}
+	}
+
+	public void remove() {
+		// TODO Auto-generated method stub
 	}
 }
